b50cae86686e21dcd62ed4c535e6f311da76a32f
roblebob <robert.koppisch@gmail.com>
2023-12-08 16:07:41 +0100

    finished 3/5

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -3,6 +3,10 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+
+memory = 0.0
 
 
 def calculator(x, oper, y):
@@ -15,20 +19,27 @@ def calculator(x, oper, y):
     elif oper == "/":
         if y == 0:
             return None
-        else:
-            return x / y
+        return x / y
 
 
-is_valid = False
-
-while not is_valid:
-
-    x, oper, y = input(msg_0).split()
-
+def assign_var(s):
+    global memory
+    if s == "M":
+        return memory
     try:
-        x = float(x)
-        y = float(y)
+        return float(s)
     except ValueError:
+        return None
+
+
+finished = False
+while not finished:
+
+    print(msg_0)
+    x, oper, y = input().split()
+
+    x, y = assign_var(x), assign_var(y)
+    if x is None or y is None:
         print(msg_1)
         continue
 
@@ -41,8 +52,29 @@ while not is_valid:
     if result is None:
         print(msg_3)
         continue
-    else:
-        is_valid = True
-        print(result)
+
+    print(result)
+
+    while True:
+        print(msg_4)
+        answer = input()
+
+        if answer == "y":
+            memory = result
+            break
+        elif answer == "n":
+            break
+
+    while True:
+        print(msg_5)
+        answer = input()
+
+        if answer == "y":
+            break
+        elif answer == "n":
+            finished = True
+            break
+
+
 
 
